61883f9315f7031e5508b4360a78d462913d250f
SJLee-0525 <sungjoon25@yonsei.ac.kr>
2025-01-17 10:17:44 +0900

    :sparkles: [Feat] 라우터 추가 (임시)

--- FE/sub-user/src/components/nav/TopNavSideElems.jsx
+++ FE/sub-user/src/components/nav/TopNavSideElems.jsx
@@ -1,12 +1,36 @@
-export default function TopNavSideElems({ imgSrc, altSrc, identifier, activeIdentifier, onClickElem }) {
+import { useNavigate } from "react-router-dom";
+
+export default function TopNavSideElems({
+  imgSrc,
+  altSrc,
+  identifier,
+  activeIdentifier,
+  onClickElem,
+}) {
+  const navigate = useNavigate();
+
   return (
     <li className="top-side-bar-nav-li">
-      <button className={activeIdentifier === identifier ? "top-side-bar-nav-elem-active" : "top-side-bar-nav-elem"} onClick={() => onClickElem(identifier)}>
+      <button
+        className={
+          activeIdentifier === altSrc
+            ? "top-side-bar-nav-elem-active"
+            : "top-side-bar-nav-elem"
+        }
+        onClick={() => {
+          onClickElem(altSrc);
+          if (altSrc === "home") {
+            navigate("/");
+          } else {
+            navigate(`/${altSrc}`);
+          }
+        }}
+      >
         <div className="top-side-bar-nav-icon">
           <img src={imgSrc} alt={altSrc} />
         </div>
         <div className="top-side-bar-nav-text">{identifier}</div>
       </button>
     </li>
-  )
+  );
 }
