da7b9f2965450bcfb9bc3d765364ef2992610faa
Seohui Hyung <ro.opretzelcham@gmail.com>
2025-02-03 17:23:11 +0900

    :art: [Style] 메인 페이지 dock 버튼 디자인 수정 및 전반 폰트 수정

--- FE/main-user/src/components/home/Dock.jsx
+++ FE/main-user/src/components/home/Dock.jsx
@@ -1,7 +1,7 @@
-import "./Home.css";
+import { useState } from "react"; //
+import "./Home.css"; //
 
 import { useContext } from "react";
-
 import { StoreContext } from "../../store/store.jsx";
 
 import alertIcon from "../../assets/alert.png";
@@ -13,6 +13,14 @@ import Icon from "./Icon.jsx";
 
 export default function Dock() {
   const store = useContext(StoreContext);
+  const [clickedIcon, setClickedIcon] = useState(null); //
+
+  // 클릭 시 애니메이션 추가
+  const handleClick = (iconType, onClickFunction) => {
+    setClickedIcon(iconType);
+    setTimeout(() => setClickedIcon(null), 200); // 0.2초 후 원래 상태로 복귀
+    onClickFunction();
+  };
 
   return (
     <div id="dock">
@@ -21,28 +29,40 @@ export default function Dock() {
         state={store.alertState}
         imgSrc={alertIcon}
         altSrc="alert"
-        onClickIcon={store.handleAlertState}
+        // onClickIcon={store.handleAlertState}
+        onClickIcon={() => handleClick("alert", store.handleAlertState)}
+        clicked={clickedIcon === "alert"}
+        disabled={!store.alertState}
       />
       <Icon
         type="dock-icon"
         state={store.cameraState}
         imgSrc={cameraIcon}
         altSrc="camera"
-        onClickIcon={store.handleCameraState}
+        // onClickIcon={store.handleCameraState}
+        onClickIcon={() => handleClick("camera", store.handleCameraState)}
+        clicked={clickedIcon === "camera"}
+        disabled={!store.cameraState}
       />
       <Icon
         type="dock-icon"
         state={store.driveState}
         imgSrc={carIcon}
         altSrc="car"
-        onClickIcon={store.handleDriveState}
+        // onClickIcon={store.handleDriveState}
+        onClickIcon={() => handleClick("car", store.handleDriveState)}
+        clicked={clickedIcon === "car"}
+        disabled={!store.driveState}
       />
       <Icon
         type="dock-icon"
         state={store.micState}
         imgSrc={micIcon}
         altSrc="microphone"
-        onClickIcon={store.handleMicState}
+        // onClickIcon={store.handleMicState}
+        onClickIcon={() => handleClick("mic", store.handleMicState)}
+        clicked={clickedIcon === "mic"}
+        disabled={!store.micState}
       />
     </div>
   );
