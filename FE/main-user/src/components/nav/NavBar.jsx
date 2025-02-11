88668b8e31cbc32c71f94b4d4b7b6819d28b13aa
Seohui Hyung <ro.opretzelcham@gmail.com>
2025-02-11 09:16:55 +0900

    :sparkles: [Feat] NavBar에 사용자 이름 출력

--- FE/main-user/src/components/nav/NavBar.jsx
+++ FE/main-user/src/components/nav/NavBar.jsx
@@ -1,11 +1,14 @@
+import { useContext } from "react";
+import Status from "./Status.jsx";
+import { UserProgressContext } from "../../store/userProgressStore.jsx";
 import "./Navbar.css";
 
-import Status from "./Status.jsx";
-
 export default function NavBar() {
+  const { loginUserInfo } = useContext(UserProgressContext);
+
   return (
     <aside id="nav-bar">
-      <p>SH</p>
+      <p>{loginUserInfo.login ? loginUserInfo.userInfo.user_name : "Guest"}</p>
       <Status />
     </aside>
   );
