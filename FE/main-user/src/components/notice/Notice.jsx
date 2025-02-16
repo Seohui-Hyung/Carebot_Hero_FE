2387e743f00927361765626616283059b089282e
Seohui Hyung <ro.opretzelcham@gmail.com>
2025-02-17 08:05:17 +0900

    :bug: [Fix] 알림창의 날짜 오류 수정

--- FE/main-user/src/components/notice/Notice.jsx
+++ FE/main-user/src/components/notice/Notice.jsx
@@ -42,19 +42,6 @@ export default function Notice({ onReply }) {
     if (koreaDate >= yesterday) return "어제";
     
     return koreaDate.toISOString().split("T")[0];
-
-    // const date = new Date(dateString);
-    // date.setHours(date.getHours() + 9);
-
-    // const today = new Date();
-    // today.setHours(today.getHours() + 9);
-
-    // const yesterday = new Date(today);
-    // yesterday.setDate(today.getDate() - 1);
-
-    // if (date.toDateString() === today.toDateString()) return "오늘";
-    // if (date.toDateString() === yesterday.toDateString()) return "어제";
-    // return date.toISOString().split("T")[0];  // YYYY-MM-DD
   };
 
   const formatTime = (dateString) => {
@@ -68,15 +55,6 @@ export default function Notice({ onReply }) {
     const formattedHours = hours % 12 || 12;
 
     return `${period} ${formattedHours}:${minutes}`;
-    // const date = new Date(dateString);
-    // date.setHours(date.getHours() + 9);
-    
-    // const hours = date.getHours();
-    // const minutes = date.getMinutes().toString().padStart(2, "0");
-    // const period = hours >= 12 ? "오후" : "오전";
-    // const formattedHours = hours % 12 || 12;
-
-    // return `${period} ${formattedHours}:${minutes}`;
   };
 
   const openNotice = (notice) => {
@@ -85,12 +63,9 @@ export default function Notice({ onReply }) {
     const extractedIndex = notice.id ? parseInt(notice.id.replace(/\D/g, ""), 10) : null;
 
     if (!notice.is_read && extractedIndex) {
-        setDisasterData((prevData) => {
-          const newData = prevData.map((item) =>
-              item.index === extractedIndex ? { ...item, is_read: true } : item
-          )
-          return [...newData];
-    });
+        setDisasterData((prevData) =>
+          prevData.map((item) => (item.index === extractedIndex ? { ...item, is_read: true } : item))
+        );
 
         markNotificationAsRead(extractedIndex);
     }
@@ -113,13 +88,13 @@ export default function Notice({ onReply }) {
           {Object.entries(groupedNotices).map(([date, notices]) => (
             <div key={date} className="notice-group">
               <h2>{date}</h2>
-              {allNotices.map((notice) => (
+              {notices.map((notice) => (
                 <NoticeBox
-                  key={notice.index}
-                  notice={{ 
-                    ...notice, 
+                  key={notice.id}
+                  notice={{
+                    ...notice,
                     time: formatTime(notice.created_at),
-                    notification_grade: notice.notification_grade
+                    notification_grade: notice.notification_grade,
                   }}
                   onClick={openNotice}
                 />
