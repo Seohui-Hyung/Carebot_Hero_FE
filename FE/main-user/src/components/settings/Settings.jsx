17961bb53fe686f686e2deb9b8445ef9ee681423
Seohui Hyung <ro.opretzelcham@gmail.com>
2025-02-17 18:15:21 +0900

    :bug: [Fix] DOM 구조 오류 처리

--- FE/main-user/src/components/settings/Settings.jsx
+++ FE/main-user/src/components/settings/Settings.jsx
@@ -3,7 +3,7 @@ import "./Settings.css";
 export default function Settings({type, children}) {
     return (
       <div id={type}>
-        {type === "box" && <p className="box-name">{children}</p>}
+        {type === "box" && <div className="box-name">{children}</div>}
       </div>
     ); 
   }
