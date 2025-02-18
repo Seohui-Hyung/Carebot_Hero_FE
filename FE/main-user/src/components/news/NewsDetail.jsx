7f775642fa67f10567558ff87440b396bb437bd0
Seohui Hyung <ro.opretzelcham@gmail.com>
2025-02-18 09:42:45 +0900

    :bug: [Fix] 터치 스크롤 이벤트 활성화

--- FE/main-user/src/components/news/NewsDetail.jsx
+++ FE/main-user/src/components/news/NewsDetail.jsx
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./News.css";
 
 export default function NewsDetail({ news, onBack }) {
+    const newsDetailRef = useRef(null);
+
+    const handleTouchStart = (e) => {
+        newsDetailRef.current.startY = e.touches[0].clientY;
+    };
+
+    const handleTouchMove = (e) => {
+        const diff = newsDetailRef.current.startY - e.touches[0].clientY;
+        newsDetailRef.current.scrollTop += diff;
+        newsDetailRef.current.startY = e.touches[0].clientY;
+    };
+  
     if (!news) {
       return <div className="news-unselected">뉴스를 선택하세요</div>;
     }
   
     return (
-      <div className="news-detail">
+      <div 
+        className="news-detail"
+        ref={newsDetailRef}
+        onTouchStart={handleTouchStart}
+        onTouchMove={handleTouchMove}
+      >
         <div className="news-content">
           <button className="back-button-detail" onClick={onBack}>←</button>
           <iframe 
