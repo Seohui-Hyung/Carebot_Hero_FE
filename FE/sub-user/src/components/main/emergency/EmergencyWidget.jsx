32c358fb8f113ea98b9bbb877af442cb6a29218d
sungjoon111 <sungjoon25@yonsei.ac.kr>
2025-02-19 20:03:34 +0900

    :bug: [Fix] 알림 없을 때 예외처리

--- FE/sub-user/src/components/main/emergency/EmergencyWidget.jsx
+++ FE/sub-user/src/components/main/emergency/EmergencyWidget.jsx
@@ -1,36 +1,33 @@
-import "./Emergency.css"
+import "./Emergency.css";
 
-import { useContext } from "react"
+import { useContext } from "react";
 
-import EmergencyWidgetAlert from "./EmergencyWidgetAlert.jsx"
-import EmergencyLog from "./EmergencyLog.jsx"
+import EmergencyWidgetAlert from "./EmergencyWidgetAlert.jsx";
+import EmergencyLog from "./EmergencyLog.jsx";
 
 // import { UserProgressContext } from "../../../store/userProgressStore.jsx";
-import { EmergencyContext } from "../../../store/emergencyStore.jsx"
+import { EmergencyContext } from "../../../store/emergencyStore.jsx";
 
 export default function EmergencyWidget() {
   // const userProgressStore = useContext(UserProgressContext);
-  const emergencyStore = useContext(EmergencyContext)
+  const emergencyStore = useContext(EmergencyContext);
 
   return (
     <>
       <div id="emergency-alert-widget">
         {emergencyStore.allNotifications.length > 0 ? (
-          emergencyStore.allNotifications.map((notification) => <EmergencyWidgetAlert key={notification.index} notification={notification} />)
+          emergencyStore.allNotifications.map((notification) => (
+            <EmergencyWidgetAlert
+              key={notification.index}
+              notification={notification}
+            />
+          ))
         ) : (
           <h3>기록된 알림이 없습니다.</h3>
         )}
       </div>
 
-      {/* <div id="emergency-widget-log">
-        <button
-          className="log-widget-button"
-          onClick={emergencyStore.handleShowAlertLog}
-        >
-          이전 기록 확인
-        </button>
-      </div> */}
       <EmergencyLog />
     </>
-  )
+  );
 }
